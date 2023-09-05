193a2682cb034d77afe64032d5f74df091cc7512
mghlb <79663822+mghlb@users.noreply.github.com>
2023-09-05 19:39:15 +0300

    Add calculation functionality to age calculator app

--- mentor/junior/age-calculator-app-main/script.js
+++ mentor/junior/age-calculator-app-main/script.js
@@ -3,13 +3,18 @@ const monthInput = document.querySelector('#month')
 const yearInput = document.querySelector('#year')
 const inputFields = document.querySelectorAll('input')
 const arrow = document.querySelector('.arrow')
+const yearValue = document.querySelector('.year__value')
+const monthValue = document.querySelector('.month__value')
+const dayValue = document.querySelector('.day__value')
 
 dayInput.addEventListener('input', () => {
   if (dayInput.value > 31 || dayInput.value < 1) {
     dayInput.parentElement.setAttribute('data-content', 'Must be a valid day')
     dayInput.parentElement.classList.add('error')
+    dayInput.classList.add('borderColor')
   } else {
     dayInput.parentElement.classList.remove('error')
+    dayInput.classList.remove('borderColor')
   }
 })
 
@@ -20,8 +25,10 @@ monthInput.addEventListener('input', () => {
       'Must be a valid month'
     )
     monthInput.parentElement.classList.add('error')
+    monthInput.classList.add('borderColor')
   } else {
     monthInput.parentElement.classList.remove('error')
+    monthInput.classList.remove('borderColor')
   }
 })
 
@@ -29,8 +36,10 @@ yearInput.addEventListener('input', () => {
   if (yearInput.value > 2024) {
     yearInput.parentElement.setAttribute('data-content', 'Must be in the past')
     yearInput.parentElement.classList.add('error')
+    yearInput.classList.add('borderColor')
   } else {
     yearInput.parentElement.classList.remove('error')
+    yearInput.classList.remove('borderColor')
   }
 })
 
@@ -43,17 +52,34 @@ arrow.addEventListener('click', () => {
         'This field is required'
       )
       inputField.parentElement.classList.add('error')
+      inputField.classList.add('borderColor')
+      return
     } else {
       inputField.parentElement.classList.remove('error')
+      inputField.classList.remove('borderColor')
       date.push(inputField.value)
     }
   })
 
   if (!isValidDate(date.join('/'))) {
     dayInput.parentElement.setAttribute('data-content', 'Must be a valid date')
-    dayInput.parentElement.classList.add('error')
+    monthInput.parentElement.setAttribute('data-content', '')
+    yearInput.parentElement.setAttribute('data-content', '')
+
+    inputFields.forEach((inputField) => {
+      inputField.parentElement.classList.add('error')
+      inputField.classList.add('borderColor')
+    })
   } else {
-    dayInput.parentElement.classList.remove('error')
+    inputFields.forEach((inputField) => {
+      inputField.parentElement.classList.remove('error')
+      inputField.classList.remove('borderColor')
+
+      const {day, month, year} = calculateAge(date.join('/'))
+      animateValue(dayValue, 0, day, 1000)
+      animateValue(monthValue, 0, month, 1000)
+      animateValue(yearValue, 0, year, 1000)
+    })
   }
 })
 
@@ -62,7 +88,7 @@ function isValidDate(date) {
     return false
   }
 
-  let [day, month, year] = date.split('/').map((p) => parseInt(p, 10))
+  let [day, month, year] = date.split('/').map((p) => parseInt(p))
   month -= 1
   const dateObj = new Date(year, month, day)
 
@@ -71,4 +97,37 @@ function isValidDate(date) {
     dateObj.getDate() === day &&
     dateObj.getFullYear() === year
   )
+}
+
+function calculateAge(date) {
+  let [dd, mm, yyyy] = date.split('/').map((p) => parseInt(p, 10))
+  mm -= 1
+  const dateObj = new Date(yyyy, mm, dd)
+  const currentDate = new Date()
+
+  const day = Math.abs(currentDate.getDate() - dateObj.getDate())
+  let month = currentDate.getMonth() - dateObj.getMonth()
+  let year = currentDate.getFullYear() - dateObj.getFullYear()
+
+  if (month < 0 || (month == 0 && currentDate.getDate() < dateObj.getDate())) {
+    year--
+    month += 12
+  }
+
+  return {day, month, year}
+}
+
+function animateValue(element, start, end, duration) {
+  let startTimestamp = null
+
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1)
+    element.textContent = Math.floor(progress * (end - start))
+    if (progress < 1) {
+      window.requestAnimationFrame(step)
+    }
+  }
+
+  window.requestAnimationFrame(step)
 }
